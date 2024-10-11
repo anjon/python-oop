f11165f36910c4a07b191f1adc718b9f10fbf8d2
Anjon Sarker <anjon.sarker@gmail.com>
2024-10-11 12:34:22 +0000

    multiple & multilevel inheritance

---
+++ multiple_inheritance.py
@@ -0,0 +1,35 @@
+# multiple inheritance = inherit from more than one parent class
+#                       C(A,B)
+# Multilevel Inheritance = inherit from a parent which inherits from another parent 
+#                       C(B) <- B(A) <- A
+
+class Animal:
+    def __init__(self, name):
+        self.name = name
+    def eat(self):
+        print(f'{self.name} is eating')
+    def sleep(self):
+        print(f'{self.name} is sleeping')
+
+class Prey(Animal):
+    def flee(self):
+        print(f'{self.name} is fleeing')
+
+class Preditor(Animal):
+    def hunt(self):
+        print(f'{self.name} is hunting')
+
+class Rabbit(Prey):
+    pass
+
+class Hawk(Preditor):
+    pass
+
+class Fish(Prey, Preditor):
+    pass
+
+rabbit = Rabbit('Bugs')
+hawk = Hawk('Tony')
+fish = Fish('Nemo')
+
+fish.eat()
